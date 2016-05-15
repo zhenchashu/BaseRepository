3916eae54d668225b147acc6d8913520ec67e237
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-05-16 00:55:26 +0800

    COMMON: Add basic headers to common.h.

---
+++ source/common/common.h
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#if       _HAS_CSTDINT_
+#include <cstdint>
+#else  // _HAS_CSTDINT_
+#include "compat_cstdint.h"
+#endif // _HAS_CSTDINT_
