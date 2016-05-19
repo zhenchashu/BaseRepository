f96bc3ed1d77302416711b555ea9b89119189be9
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-05-20 00:16:21 +0800

    COMMON: Add the thread-support header files to common.h.

--- source/common/common.h
+++ source/common/common.h
@@ -7,3 +7,12 @@
 #else  // _HAS_CSTDINT_
 #include "compat_cstdint.h"
 #endif // _HAS_CSTDINT_
+
+#ifdef    _WIN32
+#include <windows.h>
+#else  // POSIX
+#include <dlfcn.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/time.h>
+#endif // _WIN32
