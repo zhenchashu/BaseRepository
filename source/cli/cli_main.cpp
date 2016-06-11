958648cb6a16762c39f84f24470c9618514348c2
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-06-12 05:11:35 +0800

    CLI: Add a very simple thread demo after Dragon Boat Festival.

--- source/cli/cli_main.cpp
+++ source/cli/cli_main.cpp
@@ -1,6 +1,22 @@
 #include "../common/common.h"
+#include "../common/threading_wrapper.h"
+
+class TestThread : public ThreadingWrapper
+{
+public:
+  virtual void ThreadMain();
+};
+
+void TestThread::ThreadMain()
+{
+  printf("Hello World!\n\tfrom sub thread\n");
+}
 
 int main(int argc, char *argv[])
 {
+  ThreadingWrapper *test_thread = new TestThread();
+  test_thread->Start();
+  test_thread->Stop();
+
   return 0;
 }
